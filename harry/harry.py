2ad978aae1752e14e612903a51b16fe492feb81e
Zach Munro-Cape <zach.munrocape@gmail.com>
2015-01-19 20:40:42 -0400

    JMeter formatted HTTP Request generation

--- harry/harry.py
+++ harry/harry.py
@@ -32,11 +32,58 @@ Options:
 
 from docopt import docopt
 from harpy.harpy.har import Har
-import jinja2 
+from jinja2 import Environment, PackageLoader
+env = Environment(loader=PackageLoader('harry', 'templates'))
 
 
 def _verbose_print(message):
 	print message
+
+
+def generate_test_plan(har):
+	'''Return a JMeter formatted test plan from a HTTP Archive.'''
+	formatted_pages = []
+	for p in har.pages:
+		entries = har.entries_by_page_ref(p.id)
+		formatted_pages.append(generate_page(p.id, entries))
+	#print formatted_pages
+
+
+def generate_page(page_ref, entries):
+	'''Return a JMeter formatted Controller from a page_ref and its subsequent entries.'''
+	template = env.get_template('transaction.xml')
+	formatted_entries = []
+	for entry in entries:
+		formatted_entries.append(generate_entry(entry))
+	return formatted_entries
+
+
+def generate_entry(entry):
+	''' Given a HAR entry, it returns a JMeter formatted HTTP Request.'''
+	argument_template = env.get_template('argument.xml')
+	entry_template = env.get_template('request.xml')
+	formatted_arguments = []
+	for argument in entry.request.query_string:
+		formatted_arguments.append(argument_template.render(argument_name=argument.name, argument_value=argument.value))
+	
+	url_dict = extract_url_information(entry.request.url)
+	print entry_template.render(arguments=formatted_arguments, \
+								path = url_dict['path'], \
+								domain = url_dict['domain'], \
+								protocol = url_dict['protocol'], \
+								method = entry.request.method, \
+								port = 442)
+	return []
+
+
+def extract_url_information(url):
+	'''Return a dictionary containing the protocol, domain, and path of a URL.'''
+	protocol = url[:url.index(':')]
+	post_protocol = url.index('//')
+	post_domain = url.index('/', post_protocol + 2)
+	domain = url[post_protocol + 2:post_domain]
+	path = url[post_domain:]
+	return {'path':path, 'domain':domain,'protocol':protocol}
 
 
 def main():
@@ -56,7 +103,8 @@ def main():
 		except KeyError as e: # File does not conform to HTTP Archive specifications
 			print 'ERROR:'
 			print input_file + ' does not conform to the HTTP Archive standard.'
-			print 'It is missing the following key: ' + str(e).strip('\'')
+			print 'Expected the following key: ' + str(e).strip('\'')
+		generate_test_plan(har)
 	else:
 		print __doc__
 
